bf489cde928de7b388a48a6a764a58c633d368f3
Bianca Aghenitei <61416628+agheniteibianca@users.noreply.github.com>
2021-03-27 23:03:40 +0200

    Delete RestaurantManagement/javadoc file directory

--- RestaurantManagement/javadoc file/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"Business"},{"l":"Data"},{"l":"Presentation"},{"l":"Presentation.Admin"},{"l":"Presentation.Waiter"},{"l":"Start"}]
